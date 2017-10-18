6e700ed587f39c66cf55586713c1e92ba9db2f0e
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-10-18 16:59:12 -0700

    add-designers

--- src/clojure_game_geek/schema.clj
+++ src/clojure_game_geek/schema.clj
@@ -11,15 +11,35 @@
   (let [{:keys [id]} args]
     (get games-map id)))
 
+(defn resolve-board-game-designers
+  [designers-map context args board-game]
+  (->> board-game
+       :designers
+       (map designers-map)))
+
+(defn resolve-designer-games
+  [games-map context args designer]
+  (let [{:keys [id]} designer]
+    (->> games-map
+         vals
+         (filter #(-> % :designers (contains? id))))))
+
+(defn entity-map
+  [data k]
+  (reduce #(assoc %1 (:id %2) %2)
+          {}
+          (get data k)))
+
 (defn resolver-map
   []
   (let [cgg-data (-> (io/resource "cgg-data.edn")
                      slurp
                      edn/read-string)
-        games-map (->> cgg-data
-                       :games
-                       (reduce #(assoc %1 (:id %2) %2) {}))]
-    {:query/game-by-id (partial resolve-game-by-id games-map)}))
+        games-map (entity-map cgg-data :games)
+        designers-map (entity-map cgg-data :designers)]
+    {:query/game-by-id (partial resolve-game-by-id games-map)
+     :BoardGame/designers (partial resolve-board-game-designers designers-map)
+     :Designer/games (partial resolve-designer-games games-map)}))
 
 (defn load-schema
   []
