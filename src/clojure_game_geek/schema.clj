5210f9c267a0af04a2ca281e55f36cf7388892d4
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-10-18 15:12:38 -0700

    Some sample data, clean up output

--- src/clojure_game_geek/schema.clj
+++ src/clojure_game_geek/schema.clj
@@ -6,10 +6,20 @@
     [com.walmartlabs.lacinia.schema :as schema]
     [clojure.edn :as edn]))
 
+(defn resolve-game-by-id
+  [games-map context args value]
+  (let [{:keys [id]} args]
+    (get games-map id)))
+
 (defn resolver-map
   []
-  {:query/game-by-id (fn [context args value]
-                       nil)})
+  (let [cgg-data (-> (io/resource "cgg-data.edn")
+                     slurp
+                     edn/read-string)
+        games-map (->> cgg-data
+                       :games
+                       (reduce #(assoc %1 (:id %2) %2) {}))]
+    {:query/game-by-id (partial resolve-game-by-id games-map)}))
 
 (defn load-schema
   []
