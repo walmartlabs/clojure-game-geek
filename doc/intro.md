5370bc66f41b7aed15dc1349f45906d4705660c7
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-02-23 13:32:55 -0800

    Remove some cruft, update to latest lacinia libs

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clojure-game-geek
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.stuartsierra/component "0.3.2"]
-                 [com.walmartlabs/lacinia-pedestal "0.5.0"]
+                 [com.walmartlabs/lacinia "0.25.0-rc-2"]
+                 [com.walmartlabs/lacinia-pedestal "0.7.0-rc-2"]
                  [io.aviso/logging "0.2.0"]])
