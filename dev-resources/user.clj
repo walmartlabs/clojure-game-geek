7783822f9b2606d95b5db79391e60a444fd05dac
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-10-12 16:50:21 -0700

    Setup for first test

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -3,26 +3,8 @@
     [com.walmartlabs.lacinia :as lacinia]
     [clojure.java.browse :refer [browse-url]]
     [clojure-game-geek.system :as system]
-    [clojure.walk :as walk]
-    [com.stuartsierra.component :as component])
-  (:import (clojure.lang IPersistentMap)))
-
-(defn simplify
-  "Converts all ordered maps nested within the map into standard hash maps, and
-   sequences into vectors, which makes for easier constants in the tests, and eliminates ordering problems."
-  [m]
-  (walk/postwalk
-    (fn [node]
-      (cond
-        (instance? IPersistentMap node)
-        (into {} node)
-
-        (seq? node)
-        (vec node)
-
-        :else
-        node))
-    m))
+    [clojure-game-geek.test-utils :refer [simplify]]
+    [com.stuartsierra.component :as component]))
 
 (defonce system nil)
 
