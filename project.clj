17d36d49c3d2662f12c89bfdf44181c1d82dcf50
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-10-19 19:59:02 -0700

    Merge in changes (use org.clojure/java.jdbc)

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
                  [com.stuartsierra/component "0.3.2"]
                  [com.walmartlabs/lacinia "0.30.0"]
                  [com.walmartlabs/lacinia-pedestal "0.10.0"]
-                 [alaisi/postgres.async "0.8.0"]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.postgresql/postgresql "42.2.5.jre7"]
+                 [com.mchange/c3p0 "0.9.5.2"]
                  [io.aviso/logging "0.3.1"]])
